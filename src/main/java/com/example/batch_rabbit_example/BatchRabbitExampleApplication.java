23b6382a69be93bdf5b436001dcf48cbb5312330
thomas <thomas10191@hotmail.com>
2020-12-03 22:23:30 -0300

    initial commit

---
+++ src/main/java/com/example/batch_rabbit_example/BatchRabbitExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.batch_rabbit_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchRabbitExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchRabbitExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/batch_rabbit_example/BatchRabbitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batch_rabbit_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchRabbitExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
